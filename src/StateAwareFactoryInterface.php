acb279928c8be8bafa478c7d70d9bfdcc20e5e8a
Miguel Muscat <miguelmuscat93@gmail.com>
2018-07-12 11:27:56 +0200

    Added factory config param doc

--- src/StateAwareFactoryInterface.php
+++ src/StateAwareFactoryInterface.php
@@ -23,6 +23,11 @@ interface StateAwareFactoryInterface extends FactoryInterface
      *
      * @since [*next-version*]
      *
+     * @param array{
+     *     self::K_STATE:array|stdClass|ArrayObject|MapInterface
+     * }|
+     * stdClass|ArrayAccess|ContainerInterface|null $config The factory config, if any.
+     *
      * @return StateAwareInterface The created stateful instance.
      */
     public function make($config = null);
